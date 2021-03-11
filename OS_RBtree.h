670655e9daa691351ca54833fb57c1eb0e0d1589
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2021-03-12 03:28:06 +0530

    Add files via upload

---
+++ OS_RBtree.h
@@ -0,0 +1,98 @@
+/*
+    C implementation of red-black tree, taking inspiration from
+    Introduction to Algorithms (Third edition) and C++ STL.
+    Additionally supports order statistic.
+        1. order_of_key(key):   determines the no. of elements
+                                strictly smaller than a given key
+        2. find_by_order(k):    returns an iterator to kth element 
+                                (0-based indexing)
+*/
+#ifndef OS_RB_TREE_H 
+#define OS_RB_TREE_H 
+
+#define BLACK 1
+#define RED 0
+
+// Following may be defined with custom types as required prior to
+// including the header file. If so, comp_keys must also be defined.
+#ifndef KEY_TYPE
+  #define KEY_TYPE int
+  #define DEFAULT_KEY_TYPE 
+#endif
+#ifndef VALUE_TYPE
+  #define VALUE_TYPE int
+#endif
+
+typedef KEY_TYPE key_type;
+typedef VALUE_TYPE value_type;
+
+// compare function:= -1: A < B, 0: A == B, 1: A > B
+int comp_keys(const key_type A, const key_type B);
+
+typedef struct {
+    key_type key;
+    value_type val;
+} kvargs;
+
+typedef struct rbnode {
+    struct rbnode *p, *l, *r;
+    char col;
+    const key_type key;
+    value_type val;
+    int sub_sz;
+} rbnode;
+
+typedef rbnode* iterator;
+
+typedef struct {
+    int success;    // 1: inserted/erased, 0: otherwise
+    iterator it;    // iterator to insert: inserted node, erase: next node
+} insert_erase_t;
+
+typedef struct {    // constant time access to root, begin and rbegin
+    iterator root, begin, rbegin;   // begin: leftmost node
+    int size;                       // rbegin: rightmost node
+} Rb_tree;                          // size: number of nodes
+
+extern rbnode __T_nil;
+extern iterator T_nil;                                  // behaves like NULL, but typed as an iterator
+
+iterator newnode(key_type key, value_type val);         // factory method RB node
+Rb_tree newrbtree();                                    // factory method RB tree
+
+iterator next(iterator it);                             // inorder traversal, ++it in O(1) amortized, returns T_nil if not found
+iterator prev(iterator it);                             // inorder traversal, --it in O(1) amortized, returns T_nil if not found
+
+iterator find(Rb_tree *T, key_type key);                // find a key in O(logn), returns T_nil if not found
+int contains(Rb_tree *T, key_type key);                 // check existence of a key in O(logn)
+iterator lower_bound(Rb_tree *T, key_type key);         // find lower bound key of a value in O(logn), returns T_nil if not found
+iterator upper_bound(Rb_tree *T, key_type key);         // find upper bound key of a value in O(logn), returns T_nil if not found
+
+#define insert(T, ...) _insert(T, (kvargs){__VA_ARGS__})    // insert(&T, key, val) or insert(&T, key)
+insert_erase_t _insert(Rb_tree *T, kvargs kv);          // insertions in O(logn), returns {success: 0/1, iterator to newly inserted node}
+#define erase(T, x)                                     /* erase(&T, iterator) or erase(&T, key) */ \
+    _Generic((x),               \
+        iterator: _erase_it,    \
+        key_type: _erase_k      \
+    )(T, x)
+iterator _erase_it(Rb_tree *T, iterator z);             // erase(Rb_tree, iterator) in O(logn), returns iterator to next element
+insert_erase_t _erase_k(Rb_tree *T, key_type key);      // erase(Rb_tree, key) in O(logn), returns {success: 0/1, iterator to next element}
+
+// Order statistic
+#define order_of_key(T, x)                              /* order_of_key(&T, iterator) or order_of_key(&T, key) */ \
+    _Generic((x),                   \
+        iterator: order_of_key_it,  \
+        key_type: order_of_key_k    \
+    )(T, x)
+int order_of_key_it(Rb_tree *T, iterator it);           // find order statistic in O(logn)
+int order_of_key_k(Rb_tree *T, key_type key);           // returns number of elements strictly smaller than key
+iterator find_by_order(Rb_tree *T, int k);              // returns iterator to kth element (0-based) in O(logn)
+
+// Helper functions
+void fix_insert(iterator *root, iterator z);
+void fix_erase(Rb_tree *T, iterator x);
+void left_rotate(iterator *root, iterator x);
+void right_rotate(iterator *root, iterator y);
+void transplant(iterator *root, iterator x, iterator y);
+
+#endif  /* OS_RB_TREE_H */
---
+++ test_os_rbtree.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+
+// #define KEY_TYPE int       // any type may be used, int by default
+// #define VALUE_TYPE int
+#include "OS_RBtree.h"
+
+/* Provide a compare function if default key type int is not used
+int comp_keys(const key_type A, const key_type B) {
+    return A == B? 0 : A < B? -1 : +1;
+} */
+
+int main() {
+    int n = 6;
+    int a[] = {10, 5, 6, 6, -12, 30};
+    printf("Array:\n");
+    printf("%d\n", (int)sizeof(a)/sizeof *a);
+    for(int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    printf("\n\n");
+    
+    // Set operations
+    Rb_tree set = newrbtree();
+    for(int i = 0; i < n; i++)
+        insert(&set, a[i]);       // value is optional
+
+    printf("Set:\n");
+    printf("%d\n", set.size);
+    for(iterator it = set.begin; it != T_nil; it = next(it)) {        // forward iteration
+        printf("%d ", it->key);
+    } printf("\n");
+
+    if(contains(&set, 30)) {
+        printf("Set contains %d\n", 30);
+    } else {
+        printf("Set does not contain %d\n", 30);
+    }
+    if(contains(&set, 42)) {
+        printf("Set contains %d\n", 42);
+    } else {
+        printf("Set does not contain %d\n", 42);
+    }
+
+    printf("\n");
+    printf("lower_bound(%d) = %d\n", 5, lower_bound(&set, 5)->key);
+    printf("upper_bound(%d) = %d\n", 5, upper_bound(&set, 5)->key);
+    printf("lower_bound(%d) = %d\n", 17, lower_bound(&set, 17)->key);
+    printf("upper_bound(%d) = %d\n", 17, upper_bound(&set, 17)->key);
+
+    iterator it = find(&set, 6);
+    erase(&set, it);      // erase using iterator
+    erase(&set, 5);       // erase by value
+    printf("\nSet after erase(%d) and erase(%d):\n", 5, 6);
+    printf("%d\n", set.size);
+    for(iterator it = set.begin; it != T_nil; it = next(it)) {        // forward iteration
+        printf("%d ", it->key);
+    } printf("\n");
+
+    Rb_tree map = newrbtree();
+    for(int i = 0; i < n; i += 2)
+        insert(&map, a[i], a[i+1]);
+
+    printf("\n\nMap:\n");
+    printf("%d\n", map.size);
+    for(iterator it = map.begin; it != T_nil; it = next(it)) {      // forward iteration
+        printf("(%d, %d) ", it->key, it->val);
+    } printf("\n");
+
+    printf("\nmap[6]++, map[-12] = -42 and map.insert(100, 1)\n");
+    find(&map, 6)->val++;
+    find(&map, -12)->val = -42;
+    insert(&map, 100, 1);
+
+    printf("\nIn reverse order:\n");
+    printf("%d\n", map.size);
+    for(iterator it = map.rbegin; it != T_nil; it = prev(it)) {      // reverse iteration
+        printf("(%d, %d) ", it->key, it->val);
+    } printf("\n");
+
+    // ordered statistics
+    Rb_tree os = newrbtree();
+    int b[] = {-100, 1, -1, 21};
+    for(int i = 0; i < 4; i++)
+        insert(&os, b[i]);
+    
+    printf("\n\nOrdered set:\n");
+    printf("%d\n", os.size);
+    for(iterator it = os.begin; it != T_nil; it = next(it))
+        printf("%d ", it->key);
+    printf("\n");
+    printf("order_of_key(-100) = %d\n", order_of_key(&os, -100));
+    printf("order_of_key(1) = %d\n", order_of_key(&os, 1));
+    printf("order_of_key(0) = %d\n", order_of_key(&os, 0));
+    printf("order_of_key(21) = %d\n", order_of_key(&os, 21));
+    printf("order_of_key(22) = %d\n", order_of_key(&os, 22));
+    printf("\n");
+    printf("find_by_order(0) = %d\n", find_by_order(&os, 0)->key);
+    printf("find_by_order(1) = %d\n", find_by_order(&os, 1)->key);
+    printf("find_by_order(2) = %d\n", find_by_order(&os, 2)->key);
+    printf("find_by_order(3) = %d\n", find_by_order(&os, 3)->key);
+}
